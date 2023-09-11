6575485dcaea2830f35d6c4bf08c8b87243fbe0f
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-09-11 14:34:09 +0300

    add: sprite.svg, icon.jsx - component

---
+++ src/shared/Icon/Icon.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import icons from '../../assets/icons/sprite.svg';
+
+export const Icon = ({ iconName, width, height, stroke, fill }) => {
+  return (
+    <svg width={width} height={height}>
+      <use href={`${icons}#${iconName}`} stroke={stroke} fill={fill} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  stroke: PropTypes.string,
+  fill: PropTypes.string,
+};
+
+/*
+ Приклад використання компонента іконки
+      <Icon
+        iconName={'icon-arrow-left'} // назву іконки дивитись в src/assets/icons/sprite.svg і порівнювати з назвою на макеті
+        width={...}   
+        height={...} 
+        stroke={'...'} // колір обводки
+        fill={'...'} // колір заливки
+      />
+*/
