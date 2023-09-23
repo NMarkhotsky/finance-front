1180a461c9bdc16fe55f80d2f39ec626c5178999
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-09-23 11:35:58 +0300

    fix

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,9 +1,7 @@
 import { useDispatch } from 'react-redux';
-import { LoginForm } from '../Auth/LoginForm/LoginForm';
 import { Header } from '../Header/Header';
 import { Main } from './SharedLayout.styled';
 import { logout } from '../../redux/auth/operations';
-import { RegisterForm } from '../Auth/RegisterForm/RegisterForm';
 import AppRoutes from '../../routes/AppRoutes';
 
 export const SharedLayout = () => {
