ae14df96ec9a4e38e7b68d8e48bc2a6d9ea4d30b
Albina Anistratenko <albuska2013@gmail.com>
2023-10-04 18:58:10 +0300

    correct Modal

--- src/components/ModalGlobal/ModalGlobal.jsx
+++ src/components/ModalGlobal/ModalGlobal.jsx
@@ -2,52 +2,35 @@ import PropTypes from 'prop-types';
 
 import { Modal, 
   Title, 
-  Text, 
-  PetName, 
   BtnWrapper,
-  CancelButton,
-  YesButton,
+  Button,
   ButtonText,
-  IconWraper,
+  IconWrapper,
   StyledIcon, } from './ModalGlobal.styled';
-import { Trans } from 'react-i18next';
+// import { Trans } from 'react-i18next';
 import { t } from 'i18next';
 
 export const ModalGlobal = ({
     handleModal,
     handleDelete,
     title = 'Are you sure?',
-    name,
+    // name,
   }) => {
     return (
       <Modal>
+        {/* <Title>{t(title)}</Title> */}
         <Title>{title}</Title>
-        <Text>
-          <Trans i18nKey="modal_deletePet_description" values={{ name }}>
-            Are you sure you want to delete <PetName>“{name}”</PetName>? You can`t
-            undo this action.
-          </Trans>
-        </Text>
         <BtnWrapper>
-          <CancelButton onClick={handleModal}>
-            <ButtonText>{t('other_cancel')}</ButtonText>
-          </CancelButton>
-          <YesButton onClick={handleDelete}>
-            <ButtonText>{t('other_yes')}</ButtonText>
-            <IconWraper>
-              <StyledIcon iconName="icon-trash" stroke={'#FDF7F2'} />
-            </IconWraper>
-          </YesButton>
-          {/* <CancelButton onClick={handleModal}>
-            <span>Cancel</span>
-          </CancelButton>
-          <YesButton onClick={handleDelete}>
-            <ButtonText>Yes</ButtonText>
-            <IconWraper>
-              <StyledIcon iconName="icon-trash" stroke={'#FDF7F2'} />
-            </IconWraper>
-          </YesButton> */}
+          <Button onClick={handleModal}>
+            <ButtonText>{t("other_cancel")}Yes</ButtonText>
+          </Button>
+          <Button onClick={handleDelete}>
+            <ButtonText>{t("other_yes")}No</ButtonText>
+          </Button>
         </BtnWrapper>
+        <IconWrapper>
+          <StyledIcon iconName="icon-close" width="12" height="12" />
+        </IconWrapper>
       </Modal>
     );
   };
--- src/components/ModalGlobal/ModalGlobal.styled.jsx
+++ src/components/ModalGlobal/ModalGlobal.styled.jsx
@@ -1,84 +1,65 @@
 import { styled } from 'styled-components';
-import { Icon } from '../Icon/Icon';
+import { Icon } from '../../shared/components/Icon/Icon';
 import { selectTablet } from '../../utils/mediaRequest';
 
 export const Modal = styled.div`
-  width: 380px;
-  padding: 60px 12px;
-  border-radius: 20px;
-  background-color: ${({ theme }) => theme.colors.mainTextColor};
+  /* width: 380px; */
+  /* padding: 60px 12px; */
+  position: relative;
+  border-radius: 30px;
+  /* background-color: ${({ theme }) => theme.colors.mainBgdColor}; */
+  background-color: ${({ theme }) => theme.colors.greyLightTextColor};
 
   @media ${selectTablet} {
-    padding: 60px 108px;
-    width: 608px;
-    border-radius: 40px;
+    padding-top: 50px;
+    padding-bottom: 60px;
+    width: 380px;
+    height: 194px; 
   }
 `;
 
 export const Title = styled.h2`
-  margin-bottom: 14px;
-  font-family: ${props => props.theme.fonts.bold};
-  font-size: ${props => props.theme.fontSizes.xl};
-  line-height: 33px;
-  text-align: center;
-  letter-spacing: 0.04em;
-  color: ${({ theme }) => theme.colors.text};
+text-align: center;
+color: ${({ theme }) => theme.colors.mainBgdColor};
+font-size: ${props => props.theme.fontSizes.sm};
+font-family: ${props => props.theme.fonts.medium};
+letter-spacing: 0.28px;
 
-  @media ${selectTablet} {
+
+  /* @media ${selectTablet} {
     margin-bottom: 40px;
     font-size: ${props => props.theme.fontSizes.xxl};
     line-height: 49px;
-  }
+  }  */
 `;
-
-export const Text = styled.p`
-  margin-bottom: 44px;
-  font-size: ${props => props.theme.fontSizes.sm};
-  font-family: ${props => props.theme.fonts.medium};
-  color: ${({ theme }) => theme.colors.text};
-  line-height: 19px;
-  text-align: center;
-  letter-spacing: -0.14px;
-
-  @media ${selectTablet} {
-    font-size: ${props => props.theme.fontSizes.md};
-    line-height: 22px;
-  }
-`;
-
-export const PetName = styled.span`
-  font-family: ${props => props.theme.fonts.bold};
-`;
-
-
-
 
 export const BtnWrapper = styled.div`
+margin-top: 20px;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  gap: 8px;
+  align-items: center; 
+  /* gap: 15px; */
 
-  @media ${selectTablet} {
+   @media ${selectTablet} {
     flex-direction: row;
-    gap: 17px;
-  }
+    gap: 15px;
+  } 
 `;
 
 export const Button = styled.button`
-  position: relative;
-  width: 100%;
+width: 125px;
+border-radius: 16px;
+padding: 12px 0;
+border: 2px solid ${({ theme }) => theme.colors.whiteTextColor};
+
+  /* position: relative; */
+  /* width: 100%;
   padding: 8px 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border-radius: 40px;
-
-  font-size: ${props => props.theme.fontSizes.md};
-  line-height: 1.2;
-  letter-spacing: 0.64px;
-  font-family: ${props => props.theme.fonts.bold};
+  
 
   &::before {
     content: '';
@@ -93,84 +74,30 @@ export const Button = styled.button`
     background: ${({ theme }) => theme.colors.blueGradient};
     opacity: 0;
 
-    transition: opacity ${({ theme }) => theme.transitionHover};
-  }
+    transition: opacity ${({ theme }) => theme.transitionHover}; */
+  /* }
 
   @media ${selectTablet} {
     width: 129px;
     padding: 8px 20px;
-  }
+  } */
 `;
 
-export const CancelButton = styled(Button)`
-  background-color: transparent;
-  border: 2px solid ${({ theme }) => theme.colors.blue};
-  color: ${({ theme }) => theme.colors.blue};
-  transition: all ${({ theme }) => theme.transitionHover};
-
-  &:hover,
-  &:focus {
-    color: ${({ theme }) => theme.colors.white};
-    background-color: ${({ theme }) => theme.colors.blue};
-    border-color: ${({ theme }) => theme.colors.blue};
-    &::before {
-      opacity: 1;
-    }
-  }
-`;
-
-export const YesButton = styled(Button)`
-  gap: 8px;
-  padding: 6px 12px;
-  border: none;
-  background-color: ${({ theme }) => theme.colors.blue};
-  color: ${({ theme }) => theme.colors.white};
-  transition: all ${({ theme }) => theme.transitionHover};
-
-  &:hover,
-  &:focus {
-    background-color: transparent;
-    &::before {
-      opacity: 1;
-    }
-  }
-`;
-
-export const StyledPawButton = styled(Button)`
-  gap: 12px;
-  margin: 0 auto;
-  padding: 8px 28px;
-  border: none;
-  background-color: ${({ theme }) => theme.colors.blue};
-  color: ${({ theme }) => theme.colors.white};
-  transition: all ${({ theme }) => theme.transitionHover};
-
-  &:hover,
-  &:focus {
-    background-color: transparent;
-    &::before {
-      opacity: 1;
-    }
-  }
-
-  @media ${selectTablet} {
-    width: 248px;
-  }
-`;
-
-export const ButtonText = styled.span`
-  position: relative;
+export const ButtonText = styled.p`
+color: ${({ theme }) => theme.colors.mainTextColor};
+font-size: ${props => props.theme.fontSizes.xs};
+letter-spacing: 0.24px;
+text-transform: uppercase;
+font-family: ${props => props.theme.fonts.bold};
   z-index: 5;
-  font-weight: 700;
-  font-size: 16px;
 `;
 
-export const IconWraper = styled.div`
-  position: relative;
-  z-index: 5;
+export const IconWrapper = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 20px;
 `;
 
 export const StyledIcon = styled(Icon)`
-  stroke: ${({ theme }) => theme.colors.white};
-  fill: transparent;
+  stroke: ${({ theme }) => theme.colors.mainTextColor};
 `;
