96ad6bdca75f4366420fe29bde36ddf9bbfb4f05
albuska <albuska@users.noreply.github.com>
2023-10-10 17:05:06 +0000

    deploy: 9a447849f09bb22b8be629f1a117f7ec7a7c92d6

---
+++ assets/HomePage-770e6746.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-dc8a99bd.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
--- assets/ReportPage-e5beaa4e.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-a3e6771c.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Report Page"})});export{t as default};
