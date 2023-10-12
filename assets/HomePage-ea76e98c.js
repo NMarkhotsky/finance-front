04072a8b0211bf7e0f46f5549420f0290a607d5a
NMarkhotsky <NMarkhotsky@users.noreply.github.com>
2023-10-12 10:16:51 +0000

    deploy: 4048623853fb94ed6d83b801da3217a4bfac7799

---
+++ assets/HomePage-ea76e98c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a23e9256.js";const r=()=>e.jsx("div",{children:"HomePage"});export{r as default};
---
+++ assets/ReportPage-2f0c3af1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-a23e9256.js";const t=()=>e.jsx("div",{children:e.jsx("h1",{children:"Report Page"})});export{t as default};
