7f1601bc0a21802159a678b4fcf4c3825caa77bc
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-10-12 13:03:36 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import { fonts } from './constants';
