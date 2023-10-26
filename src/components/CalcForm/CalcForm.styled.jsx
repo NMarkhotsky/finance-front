84e7d34f69642c15045f5d7539f3660dd1ae679a
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-10-26 12:59:15 +0300

    Update CalcForm.styled.jsx

--- src/components/CalcForm/CalcForm.styled.jsx
+++ src/components/CalcForm/CalcForm.styled.jsx
@@ -9,6 +9,8 @@ export const CalcContainer = styled.div`
     position: absolute;
     top: 0;
     left: 0;
+    right: 0;
+    bottom: 0;
 `
 
 export const CalcStyledForm = styled.form`
@@ -18,8 +20,8 @@ export const CalcStyledForm = styled.form`
     font-size: 16px;
     position: absolute;
     top: 320px;
-    right: 50%;
-    transform: translate(-50%, 0);
+    /* right: 50%; */
+    transform: translate(50%, 0);
     border-radius: 16px 0 16px 16px;
     box-shadow: 5px 10px 20px 0px rgba(170, 178, 197, 0.40);
 
