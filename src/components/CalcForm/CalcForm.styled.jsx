cc73ef2291149203c72bb538a18c98a15a713dee
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-10-26 12:59:35 +0300

    Update CalcForm.styled.jsx

--- src/components/CalcForm/CalcForm.styled.jsx
+++ src/components/CalcForm/CalcForm.styled.jsx
@@ -7,10 +7,6 @@ export const CalcContainer = styled.div`
     height: 100vh;
     background-color: transparent;
     position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
 `
 
 export const CalcStyledForm = styled.form`
