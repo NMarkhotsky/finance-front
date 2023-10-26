1032de3a56f34fd473deb06a37148525def0d093
albuska <albuska@users.noreply.github.com>
2023-10-26 16:53:12 +0000

    deploy: 2afeda3cf1d2610acbde12239bf067690475059c

---
+++ assets/ReportPage-16a9c4bb.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-a78fe388.js";const r=()=>(console.log("Hello"),e.jsxs("div",{children:[e.jsx("h1",{children:"Report Page"}),e.jsx("h2",{children:"Hello world"})]}));export{r as default};
