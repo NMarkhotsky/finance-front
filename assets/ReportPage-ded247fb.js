e6f31002b63a872e0d814b49009d32f0157dd9a2
AllaGrey <AllaGrey@users.noreply.github.com>
2023-10-27 20:56:23 +0000

    deploy: c8bdcf5b2d95c3da8eaa3c350079f8aa301386b1

--- assets/ReportPage-ded247fb.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./index-41732584.js";const r=()=>(console.log("Hello"),e.jsxs("div",{children:[e.jsx("h1",{children:"Report Page"}),e.jsx("h2",{children:"Hello world"})]}));export{r as default};
