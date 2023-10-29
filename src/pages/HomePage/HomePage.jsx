a27d0fd2bfffd833f2fd57baa8bf0426f00a14c0
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-10-29 18:07:45 +0200

    add components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import { AddTransaction } from "../../components/AddTransaction/AddTransaction";
 import { BalanceForm } from "../../components/BalanceForm/BalanceForm";
+import { ButtonToReports } from "../../components/ButtonToReports/ButtonToReports";
 import { CurrentDay } from "../../components/CurrentDay/CurrentDay";
 import { InfoBox } from "../../components/InfoBox/InfoBox";
 import { TransactionTable } from "../../components/TransactionTable/TransactionTable";
@@ -7,6 +8,7 @@ import { TransactionTable } from "../../components/TransactionTable/TransactionT
 const HomePage = () => {
   return <div>
     <BalanceForm />
+    <ButtonToReports/>
     <InfoBox />
     <CurrentDay/>
     <AddTransaction />
--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,8 +1,10 @@
 import { BarChartComp } from "../../components/BarChart/BarChart";
+import { BackButton } from "../../components/BackButton/BackButton";
 
 const ReportPage = () => {
   return (
     <div>
+      <BackButton/>
       <BarChartComp/>
     </div>
   );
