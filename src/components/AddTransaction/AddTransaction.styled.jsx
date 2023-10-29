857321f2b298f4a7cd4baead824c3c51e3c33c1c
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-10-29 17:26:51 +0200

    Update AddTransaction.styled.jsx

--- src/components/AddTransaction/AddTransaction.styled.jsx
+++ src/components/AddTransaction/AddTransaction.styled.jsx
@@ -14,6 +14,7 @@ export const DataWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 51px;
 
     @media ${selectTablet} {
         flex-direction: row;
