64189764e3c145a44ebbfe0a724a25d19627b339
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-03 21:30:38 +0200

    correct

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -1,12 +1,14 @@
 import { BarChartComp } from "../../components/BarChart/BarChart";
 import { BackButton } from "../../components/BackButton/BackButton";
 import { SelectCurrentPeriod } from "../../components/SelectCurrentPeriod/SelectCurrentPeriod";
+import { MenuForActiveLinkOnReport } from "../../components/MenuForActiveLinkOnReport/MenuForActiveLinkOnReport";
 
 const ReportPage = () => {
   return (
     <div>
       <BackButton/>
       <SelectCurrentPeriod/>
+      <MenuForActiveLinkOnReport/>
       <BarChartComp/>
     </div>
   );
