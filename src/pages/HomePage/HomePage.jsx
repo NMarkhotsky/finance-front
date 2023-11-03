410159e374e3e95010d57f448f954c2359b34960
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-03 20:20:17 +0200

    correct

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,14 @@ import { BalanceForm } from "../../components/BalanceForm/BalanceForm";
 import { ButtonToReports } from "../../components/ButtonToReports/ButtonToReports";
 import { CurrentDay } from "../../components/CurrentDay/CurrentDay";
 import { InfoBox } from "../../components/InfoBox/InfoBox";
-// import { MenuForActiveLink } from "../../components/MenuForActiveLink/MenuForActiveLink";
+import { MenuForActiveLink } from "../../components/MenuForActiveLink/MenuForActiveLink";
 import { TransactionTable } from "../../components/TransactionTable/TransactionTable";
 
 const HomePage = () => {
   return <div>
     <BalanceForm />
     <ButtonToReports/>
-    {/* <MenuForActiveLink/> */}
+    <MenuForActiveLink/>
     <InfoBox />
     <CurrentDay/>
     <AddTransaction />
