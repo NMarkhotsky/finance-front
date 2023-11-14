8e2013f76171eb9f66f62df83ee3284d5796310e
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-14 21:06:56 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,10 @@
-// import { AddTransaction } from "../../components/AddTransaction/AddTransaction";
 import { BalanceForm } from "../../components/BalanceForm/BalanceForm";
 import { ButtonToReports } from "../../components/ButtonToReports/ButtonToReports";
-// import { CurrentDay } from "../../components/CurrentDay/CurrentDay";
 import { InfoBox } from "../../components/InfoBox/InfoBox";
 import { MenuForActiveLink } from "../../components/MenuForActiveLink/MenuForActiveLink";
 import { Container } from "./HomePage.styled";
-// import { TransactionTable } from "../../components/TransactionTable/TransactionTable";
 
 const HomePage = () => {
-  // return <div>
-  //   <BalanceForm />
-  //   <ButtonToReports/>
-  //   <MenuForActiveLink/>
-  //   <InfoBox />
-  //   <CurrentDay/>
-  //   <AddTransaction />
-  //   <TransactionTable/>
-  // </div>;
   return (
     <>
       <Container>
