7ff1f192d0a2e1fa2437cae427f4abcd481d9257
albuska <albuska@users.noreply.github.com>
2023-11-17 09:54:40 +0000

    deploy: 5d00a22d2a3f58097b9f5e5caf5a3d5b4cff40a8

--- assets/ReportPage-a9915932.js
+++ assets/ReportPage-a9915932.js
@@ -1,4 +1,4 @@
-import{c as Je,L as Hv,j as U,I as vn,l as Xh,r as L,b as oe,z as da,A as qv,D as $,P as K,q as Gv}from"./index-7f0b8a01.js";import{b as Kv,c as Yv}from"./expensesApi-192260b9.js";const Xv=Je(Hv)`
+import{c as Je,L as Hv,j as U,I as vn,l as Xh,r as L,b as oe,z as da,A as qv,D as $,P as K,q as Gv}from"./index-5e6ed1fa.js";import{b as Kv,c as Yv}from"./expensesApi-fc8704dd.js";const Xv=Je(Hv)`
 display: flex;
 align-items: center;
 gap: 16px;
