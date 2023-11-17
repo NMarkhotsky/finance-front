e9e8e85273c0bb9d6699527f1c5269bbeafa4a2e
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-17 11:41:47 +0200

    correct

---
+++ src/shared/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import { ImageLoader } from "./Loader.styled";
+// import Img from "../../../assets/images/cabbage.png";
+
+export const Loader = () => {
+
+  return (
+    <>
+      {/* <ImageLoader src={Img} alt="Cabbage" /> */}
+      <ImageLoader iconName="icon-cabbage"/>
+    </>
+  );
+};
