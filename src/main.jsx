cef58188d21c715a2242000f9e6373c885e3ba95
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-24 21:01:40 +0200

    add Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
-import App from './App.jsx';
-import { Loader } from './shared/components/Loader/Loader.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
+import { Loader } from "./shared/components/Loader/Loader.jsx";
+import { MyContextProvider } from "./utils";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/finance-front">
       <Provider store={store}>
         <PersistGate loading={<Loader />} persistor={persistor}>
-        <React.Suspense fallback={<Loader />}>
-            <App />
-          </React.Suspense>
+          <MyContextProvider>
+            <React.Suspense fallback={<Loader />}>
+              <App />
+            </React.Suspense>
+          </MyContextProvider>
         </PersistGate>
       </Provider>
     </BrowserRouter>
