189a083e0f31f84d180ebf330db90d488e5b437a
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-11-25 16:44:43 +0200

    Update authApi.js

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -26,7 +26,7 @@ axios.interceptors.response.use(
         console.log(response, 'prevRequest.response?.status ? == 401  response');
         if (response) {
           authHeader.set(response.data.token)
-          prevRequest.headers.Authorization = `Bearer ${response.data.token}`
+          prevRequest.config.headers.Authorization = `Bearer ${response.data.token}`
         }
         return axios(prevRequest)
       } catch (refreshError) {
