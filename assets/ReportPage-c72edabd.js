f09b626cfff57c3bb62ab2ee9e276c2ea38c1fcb
AllaGrey <AllaGrey@users.noreply.github.com>
2023-11-25 15:13:51 +0000

    deploy: f382cfe7eda8b1bee9e3a095090f5c6cca493f62

--- assets/ReportPage-c72edabd.js
+++ assets/ReportPage-c72edabd.js
@@ -1,4 +1,4 @@
-import{s as ae,b as Qv,L as Jv,j as B,I as Qt,d as Jd,P as z,r as I,n as ey,p as eh,q as ue,t as pa,v as ty,w as $,g as ry,k as ny,C as ay,a as ta}from"./index-e2320e5c.js";import{f as $o,l as iy,m as oy,p as uy,q as sy,h as cy,B as ly}from"./expensesApi-d0137717.js";const fy=ae(Jv)`
+import{s as ae,b as Qv,L as Jv,j as B,I as Qt,d as Jd,P as z,r as I,n as ey,p as eh,q as ue,t as pa,v as ty,w as $,g as ry,k as ny,C as ay,a as ta}from"./index-eaf8a755.js";import{f as $o,l as iy,m as oy,p as uy,q as sy,h as cy,B as ly}from"./expensesApi-dcb5b30b.js";const fy=ae(Jv)`
 display: flex;
 align-items: center;
 gap: 5px;
