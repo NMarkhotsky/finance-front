bd8cf4fc6cc7c3899e180a0b0e31dc9c5114703e
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-11-25 17:17:52 +0200

    Update authApi.js

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -28,7 +28,7 @@ axios.interceptors.response.use(
           authHeader.set(response.data.token)
           console.log(response.data.token, 'newww token!!!!');
           prevRequest.config.headers.Authorization = `Bearer ${response.data.token}`
-          prevRequest.config.url = '/auth/current'
+          prevRequest.config.baseURL = 'https://finance-backend-eight.vercel.app/api/auth/current'
           return axios(prevRequest)
         }
         
